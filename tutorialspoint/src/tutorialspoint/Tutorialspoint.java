587227d387c33e8e6cd3373b55a287232f7fee80
JavaKurt <kurtis.009@hotmail.com>
2020-01-10 18:49:35 +0000

    testing

---
+++ tutorialspoint/src/tutorialspoint/Tutorialspoint.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorialspoint;
+
+/**
+ *
+ * @author Kurtis
+ */
+public class Tutorialspoint {
+    public static void main(String[] args){
+        
+        String str = new String("Welcome to Tutorialspoint.com");
+        
+        System.out.print("Return value: ");
+        System.out.println(str.toUpperCase());
+    }
+    
+    
+}
